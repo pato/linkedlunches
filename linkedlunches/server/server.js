1127f3a14ab6e97c37789bb6812e4b6f5ff2d622
Pato <plankenau@gmail.com>
2015-03-30 13:19:13 -0500

    added event and people structure

--- linkedlunches/server/server.js
+++ linkedlunches/server/server.js
@@ -1,1 +1,29 @@
 Events = new Mongo.Collection("events");
+People = new Mongo.Collection("people");
+
+/**
+ * Event structure
+ * {
+ *  people: {},
+ *  max_people: int,
+ *  location: string,
+ *  datetime: string,
+ *  url: string,
+ *  interest: string,
+ * }  
+ */
+
+/**
+ * People structure
+ * {
+ *  name: string.
+ *  email: string,
+ *  fb_auth: string,
+ *  linkedin_auth: string.
+ *  food_preferances: {},
+ *  location: string,
+ *  times: {},
+ *  phone_number: string
+ *  events: {}
+ * }
+ */
